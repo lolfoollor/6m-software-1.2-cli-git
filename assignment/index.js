72ca33098fd9c111033df9e5f17a383815cc5542
Lolfoollor <L_Z_F47@hotmail.com>
2024-06-04 20:39:17 +0800

    Add draggable event for cycling navigation

--- assignment/index.js
+++ assignment/index.js
@@ -1,6 +1,49 @@
+const radioBtns = document.querySelectorAll('input[type="radio"]');
+const numRadioBtn = radioBtns.length;
 let scrollToTopButton = document.getElementById('return-to-top');
-const labels = document.querySelectorAll('.button');
+const buttonLabels = document.querySelectorAll('.button');
+const contentContainers = document.querySelectorAll('.content-container');
+let currRadioButton;
 
+/*Init*/
+updateNavigationBtn(buttonLabels, "radio1");
+
+function updateNavigationBtn(buttonLabels, targetRadioId) {
+    targetId = parseInt(targetRadioId.slice(-1));
+    if (!targetRadioId.startsWith('radio') || currRadioButton === targetId) return;
+    currRadioButton = parseInt(targetId);
+    buttonLabels.forEach(buttonLabel => buttonLabel.classList.remove('outline'));
+
+    const buttonLabelArr = [...buttonLabels];
+    let radioButton = document.getElementById(targetRadioId);
+    radioButton.checked = true;
+    const targetRadioButtonsLabel = buttonLabelArr.filter(buttonLabel => buttonLabel.getAttribute('for') === targetRadioId);
+    targetRadioButtonsLabel.forEach(radioButtonsLabel => radioButtonsLabel.classList.add('outline'));
+}
+
+/*Disable repeat function for Xms*/
+function debounce(func, wait = 1000) {
+    let timeout;
+    let isPending = false;
+  
+    return (...args) => {
+      if (isPending) return;
+      isPending = true;
+      func.apply(this, args);
+  
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        isPending = false;
+      }, wait);
+    };
+  }
+
+buttonLabels.forEach(buttonLabel => {
+    buttonLabel.addEventListener('click', function() {
+        const forAttr = buttonLabel.getAttribute('for');
+        updateNavigationBtn(buttonLabels, forAttr)
+    });
+});
 
 scrollToTopButton.addEventListener('click', function() {
     window.scrollTo({
@@ -9,17 +52,38 @@ scrollToTopButton.addEventListener('click', function() {
     });
 });
 
-labels.forEach(label => {
-    label.addEventListener('click', function() {
-        const forAttr = label.getAttribute('for');
-        labels.forEach(label => {
-            label.classList.remove('outline');
-        });
-        labels.forEach(label => {
-            const labelForAttr = label.getAttribute('for');
-            if (labelForAttr === forAttr) {
-                label.classList.add('outline');
+contentContainers.forEach(contentContainer => {
+    let startX = 0;
+    let isDragging = false;
+
+    contentContainer.addEventListener('pointerdown', (e) => {
+        startX = e.clientX;
+        isDragging = true;
+        console.log("mousedown", isDragging);
+    });
+
+    document.addEventListener('pointermove', debounce((e) => {
+        if (!isDragging) return;
+
+        const diffX = e.clientX - startX;
+        console.log("mousemove", diffX);
+
+        if (Math.abs(diffX) > 5) {
+            let nextId; 
+            if (diffX > 0) {
+                nextId = currRadioButton % numRadioBtn + 1;
+            } else {
+                nextId = (currRadioButton === 1) ? numRadioBtn : currRadioButton - 1;
             }
-        });
+            let nextButtonId = "radio" + nextId;
+            updateNavigationBtn(buttonLabels, nextButtonId);
+            startX = e.clientX;
+        };
+    }));
+
+    document.addEventListener('pointerup', () => {
+        console.log("mouseup");
+        isDragging = false;
     });
 });
+
