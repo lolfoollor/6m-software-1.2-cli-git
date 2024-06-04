b1d44c572f97f1d7dc7272991775aea4eeec5793
Lolfoollor <L_Z_F47@hotmail.com>
2024-06-04 21:15:19 +0800

    Fix transitioning bug

--- assignment/index.js
+++ assignment/index.js
@@ -4,6 +4,7 @@ let scrollToTopButton = document.getElementById('return-to-top');
 const buttonLabels = document.querySelectorAll('.button');
 const contentContainers = document.querySelectorAll('.content-container');
 let currRadioButton;
+let isTransitioning = false;
 
 /*Init*/
 updateNavigationBtn(buttonLabels, "radio1");
@@ -21,23 +22,6 @@ function updateNavigationBtn(buttonLabels, targetRadioId) {
     targetRadioButtonsLabel.forEach(radioButtonsLabel => radioButtonsLabel.classList.add('outline'));
 }
 
-/*Disable repeat function for Xms*/
-function debounce(func, wait = 1000) {
-    let timeout;
-    let isPending = false;
-  
-    return (...args) => {
-      if (isPending) return;
-      isPending = true;
-      func.apply(this, args);
-  
-      clearTimeout(timeout);
-      timeout = setTimeout(() => {
-        isPending = false;
-      }, wait);
-    };
-  }
-
 buttonLabels.forEach(buttonLabel => {
     buttonLabel.addEventListener('click', function() {
         const forAttr = buttonLabel.getAttribute('for');
@@ -54,21 +38,22 @@ scrollToTopButton.addEventListener('click', function() {
 
 contentContainers.forEach(contentContainer => {
     let startX = 0;
+    let startY = 0;
     let isDragging = false;
 
     contentContainer.addEventListener('pointerdown', (e) => {
         startX = e.clientX;
+        startY = e.clientY;
         isDragging = true;
-        console.log("mousedown", isDragging);
     });
 
-    document.addEventListener('pointermove', debounce((e) => {
-        if (!isDragging) return;
+    document.addEventListener('pointermove', (e) => {
+        if (!isDragging || isTransitioning) return;
 
         const diffX = e.clientX - startX;
-        console.log("mousemove", diffX);
+        const diffY = e.clientY - startY;
 
-        if (Math.abs(diffX) > 5) {
+        if (Math.abs(diffX) > 300 && Math.abs(diffY) *  2 <= Math.abs(diffX)) {
             let nextId; 
             if (diffX > 0) {
                 nextId = currRadioButton % numRadioBtn + 1;
@@ -76,14 +61,27 @@ contentContainers.forEach(contentContainer => {
                 nextId = (currRadioButton === 1) ? numRadioBtn : currRadioButton - 1;
             }
             let nextButtonId = "radio" + nextId;
+            isTransitioning = true;
             updateNavigationBtn(buttonLabels, nextButtonId);
+            setTimeout(() => {
+                isTransitioning = false;
+            }, 1000);
             startX = e.clientX;
+            startY = e.clientY;
+            
+            /* Remove any Selection accidently cause by dragging */
+            const selection = window.getSelection();
+            if (selection.rangeCount > 0) {
+                selection.removeAllRanges();
+            }
         };
-    }));
+    });
 
     document.addEventListener('pointerup', () => {
-        console.log("mouseup");
+        isDragging = false;
+    });
+
+    document.addEventListener('pointerleave', () => {
         isDragging = false;
     });
 });
-
